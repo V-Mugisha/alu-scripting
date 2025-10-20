f5a795432048d4eb1dca8556db2d6594fb463f05
Victor Mugisha <victormugishavm6@gmail.com>
2025-10-20 21:08:25 +0200

    Updated 1-to_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""
+1-top_ten
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    Args:
+        subreddit (str): The name of the subreddit.
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
+    Returns:
+        None: Prints titles or None if invalid.
+    """
+    if not subreddit:
+        print(None)
+        return
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'MyRedditBot/0.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    try:
+        data = response.json()
+        posts = data['data']['children'][:10]
+        for post in posts:
+            print(post['data']['title'])
+    except (KeyError, ValueError):
         print(None)
