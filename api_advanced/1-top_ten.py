7a84917a77b1aaf0ffc1d41339c56025166f0d85
Victor Mugisha <victormugishavm6@gmail.com>
2025-10-20 21:12:23 +0200

    Updated 1-to_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,25 +4,21 @@ import requests
 
 
 def top_ten(subreddit):
-   """Return number of subscribers if @subreddit is valid subreddit.
+    """Return number of subscribers if @subreddit is valid subreddit.
     if not return 0."""
-   
-    if not subreddit:
-        print(None)
-        return
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'MyRedditBot/0.1'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
-    if response.status_code != 200:
-        print(None)
-        return
-
-    try:
-        data = response.json()
-        posts = data['data']['children'][:10]
-        for post in posts:
-            print(post['data']['title'])
-    except (KeyError, ValueError):
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
         print(None)
